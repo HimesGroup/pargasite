007ae551022d73956d04706682d6f212596b22a4
Nisha Narayanan <nishan@seas.upenn.edu>
2023-04-05 00:58:23 -0400

    Update readme

--- README.md
+++ README.md
@@ -1,27 +1,34 @@
+
 # Pollution-Associated Risk Geospatial Analysis SITE (pargasite)
 
-Authors: Rebecca Greenblatt, Avantika Diwadkar, Nisha Narayanan, Blance Himes
+Contributors: Nisha Narayanan, Avantika Diwadkar, Rebecca Greenblatt, Blanca Himes
 
-Pollution-Associated Risk Geospatial Analysis SITE (PARGASITE) is an online web-application and R package that can be used to estimate levels of pollutants in the U.S. for 1997 through 2021 at user-defined geographic locations and time ranges. Measures correspond to monthly and yearly raster files (Jan 2005 to Dec 2021) for PM2.5, Ozone, NO2, SO2, and CO covering the US and Puerto Rico that were created from United States Environmental Protection Agency (EPA) regulatory monitor data. The R package allows the user to obtain more customized output as well as work with the raster layers directly.
+Pollution-Associated Risk Geospatial Analysis SITE (PARGASITE) is an online web application and R package that can be used to estimate levels of pollutants in the U.S. for 1997 through 2021 at user-defined geographic locations and time ranges. Measures correspond to monthly and yearly raster files for PM2.5, Ozone, NO2, SO2, and CO covering the contiguous United States and Puerto Rico that were created with U.S. Environmental Protection Agency (EPA) regulatory monitor data. The R package allows users to obtain more customized output and work with the raster layers directly. 
 
-In this repository you will first find a shiny app (/app), a version of which is also hosted on pargasite.org. 
+In this repository you will first find a shiny app (/app), a version of which is also hosted on https://pargasite.org/. 
 
 You will also find an R package (/pkg) that can be downloaded with devtools::install_github("HimesGroup/pargasite", subdir = "pkg"). You can refer to files in the examples folder which help illustrate the use of all functions in this package. 
 
 Lastly, you will find the code we used to generate the data used in both the website and the package.
 
-Pargasite uses publicly available data from the United States Environmental Protection Agency (EPA). We have no affiliation with the EPA. 
-From this data, we generated the monthly and yearly raster files (Jan 2005 to Dec 2021) for PM2.5, Ozone, NO2, SO2, and CO using inverse distance weighted interpolation from the 5 nearest EPA monitoring stations.
+## Data 
 
-Please not that the website and R package provide similar functionality. Their specific contributions include:
-- the website provides visualization of the raster layers as well as the ability to upload a geocoded dataset then download one with pargasite estimates without interfacing with R.  
-- the R package allows the user to obtain more customized output as well as work with the raster layers directly 
+Pargasite uses publicly available data from the United States Environmental Protection Agency (EPA). We have no affiliation with the EPA. From this data, we generated the monthly and yearly raster files (Jan 2005 to Dec 2021) for PM2.5, Ozone, NO2, SO2, and CO using inverse distance weighted interpolation from the 5 nearest EPA monitoring stations.
 
-Both tools were made to assist with analyses of geocoded health data to study the association between pollution exposure and health outcomes.
+## Additional Information
 
-Notes:
-- the raster layers accessible in the R package are not cropped to fit the United States, and instead are in the shape of a rectangle covering the region. 
-- unit of measurement is displayed in the shiny app once a pollutant is selected. In the package, use getUnits() to display a list of the units.
-- the first time a raster brick is loaded in the package, it will take a while because the data has to be retrieved from online. However, the raster layer will be stored in a temporary file, so it will load much more quickly thereafter.
+The website and R package are similar, but some of their specific functions include:
 
-Reference: Greenblatt RE, Himes BE. Facilitating Inclusion of Geocoded Pollution Data into Health Studies. AMIA Jt Summits Transl Sci Proc. 2019;2019:553–561.(PMID: 31259010)
+1) The website provides visualization of the raster layers as well as the ability to upload a file with geocodes to then download one with PARGASITE estimates without interfacing with R.
+
+2) The unit of measurement is displayed in the Shiny app according to the pollutant selected. To display units with the R package, use the getUnits() function.
+
+3)  The first time a raster layer is loaded in a user’s R environment by the R package, it will take some time to retrieve the data online. However, the raster layer will be stored in a temporary file that will improve subsequent loading times.
+
+4)  The latest updates to the app allows the user to visualize and download the pollutant data at the MMSA and County level.
+
+5) The latest updates to the R package allows the user to work with the raster layers and download the pollutant values at MMSA, Counties, Census Tracts, and ZIPCODE level. Example use cases can be found at /pkg/examples/.
+
+##Reference
+
+Greenblatt RE, Himes BE. Facilitating Inclusion of Geocoded Pollution Data into Health Studies. AMIA Jt Summits Transl Sci Proc. 2019;2019:553–561.(PMID: 31259010)
--- app/ui.R
+++ app/ui.R
@@ -2,77 +2,121 @@
 #.libPaths("/home/maya/R/x86_64-pc-linux-gnu-library/3.4/")
 
 library(leaflet)
+library(shinyWidgets)
 
 shinyUI(fluidPage(theme = "bootstrap.css",
-                  
-                  tags$head(
-                    tags$style(HTML("
-                                    h1 {
-                                    font-weight: 1000;
-                                    line-height: 2;
-                                    color: #fff;
-                                    }
-
-                                    "))
-                  ),
-                  
-                  headerPanel("Pollution-Associated Risk Geospatial Analysis SITE (PARGASITE)"
-                  ),
-                  mainPanel(
-                    leafletOutput("map", width = "100%",height= 600),
-                    p("Data source: United States Environmental Protection Agency (EPA). <aqs.epa.gov/aqsweb/airdata.download_files.html>")),
-                    
-                  br(),
-                  sidebarPanel(
-                    h4("Map View"),
-                    selectizeInput(inputId = "year",
-                                   label = "Year",
-                                   choices = c("1997":"2019"),
-                                   selected = "2017",
-                                   multiple = FALSE),
-                    selectizeInput(inputId = "pollutant",
-                                   label = "Pollutant",
-                                   choices = c("PM2.5", "Ozone", "NO2", "SO2", "CO"),
-                                   select = "PM2.5",
-                                   multiple = FALSE),
-                    p(textOutput("notes")),
-                    h5(textOutput("latlong")),
-                    h5(textOutput("pollutant_val")),
-                    hr(),
-                    h4("Upload dataset to get corresponding pollution estimates"),
-                    p("The file returned will have a column for each pollutant; the value will be an average of monthly estimates over the specified time period."),
-                    fluidRow(
-                      column(6, selectizeInput('start_month', label = "Start month",
-                                               choices = c("Jan", "Feb", "March", "April", "May",
-                                                           "June", "July", "Aug", "Sept", "Oct",
-                                                           "Nov", "Dec"),
-                                               selected = "Jan",
-                                               multiple = FALSE)),
-                      column(6, selectizeInput(inputId = "start_year",
-                                               label = "Start Year",
-                                               choices = c("1997":"2019"),
-                                               selected = "2015",
-                                               multiple = FALSE))),
-                    fluidRow(
-                      column(6, selectizeInput('end_month', label = "End month",
-                                               choices = c("Jan", "Feb", "March", "April", "May",
-                                                           "June", "July", "Aug", "Sept", "Oct",
-                                                           "Nov", "Dec"),
-                                               selected = "March",
-                                               multiple = FALSE)),
-                      column(6, selectizeInput(inputId = "end_year",
-                                               label = "End Year",
-                                               choices = c("1997":"2019"),
-                                               selected = "2015",
-                                               multiple = FALSE))),
-                    h5(p("Choose .csv file with Latitude and Longitude columns. A sample input file can be downloaded",downloadLink("downloadData", "here."))),
-                    fileInput("user_file", " ",
-                              multiple = FALSE,
-                              accept = c("text/csv",
-                                         "text/comma-separated-values,
+                  setBackgroundColor("ghostwhite"),
+                  tags$style(HTML("body {line-height: 1.75}")),
+                  title= "Pollution-Associated Risk Geospatial Analysis SITE (PARGASITE)",
+                  titlePanel(h1(HTML(paste(h1(style = "color:black", "Pollution-Associated Risk Geospatial Analysis SITE (PARGASITE)"))), align = "left")),
+                  tabsetPanel(
+                    tabPanel(title = HTML(paste(h3(style = "color:black","USA"))), 
+                             sidebarPanel(
+                               h4("Map View"),
+                               selectizeInput(inputId = "year",
+                                              label = "Year",
+                                              choices = c("1997":"2021"),
+                                              selected = "2021",
+                                              multiple = FALSE),
+                               selectizeInput(inputId = "pollutant",
+                                              label = "Pollutant",
+                                              choices = c("PM2.5", "Ozone", "NO2", "SO2", "CO"),
+                                              select = "PM2.5",
+                                              multiple = FALSE),
+                               p(textOutput("notes")),
+                               h5(textOutput("latlong")),
+                               h5(textOutput("pollutant_val")),
+                               hr(),
+                               h4("Upload dataset to get corresponding pollution estimates"),
+                               p("The file returned will have a column for each pollutant; the value will be an average of monthly estimates over the specified time period."),
+                               fluidRow(
+                                 column(6, selectizeInput('start_month', label = "Start month",
+                                                          choices = c("Jan", "Feb", "March", "April", "May",
+                                                                      "June", "July", "Aug", "Sept", "Oct",
+                                                                      "Nov", "Dec"),
+                                                          selected = "Jan",
+                                                          multiple = FALSE)),
+                                 column(6, selectizeInput(inputId = "start_year",
+                                                          label = "Start Year",
+                                                          choices = c("1997":"2021"),
+                                                          selected = "2015",
+                                                          multiple = FALSE))),
+                               fluidRow(
+                                 column(6, selectizeInput('end_month', label = "End month",
+                                                          choices = c("Jan", "Feb", "March", "April", "May",
+                                                                      "June", "July", "Aug", "Sept", "Oct",
+                                                                      "Nov", "Dec"),
+                                                          selected = "March",
+                                                          multiple = FALSE)),
+                                 column(6, selectizeInput(inputId = "end_year",
+                                                          label = "End Year",
+                                                          choices = c("1997":"2021"),
+                                                          selected = "2015",
+                                                          multiple = FALSE))),
+                               h5(p("Choose .csv file with Latitude and Longitude columns. A sample input file can be downloaded",downloadLink("downloadData", "here."))),
+                               fileInput("user_file", " ",
+                                         multiple = FALSE,
+                                         accept = c("text/csv",
+                                                    "text/comma-separated-values,
                                          text/plain",
-                                         ".csv")),
-                    downloadButton("finalDownload", "Download"),hr(),
-                    h6(p("Greenblatt RE, Himes BE. Facilitating Inclusion of Geocoded Pollution Data into Health Studies. AMIA Jt Summits Transl Sci Proc. 2019;2019:553–561.(PMID:",
-                       a("31259010",href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6568125/",target="_blank"),").", a("GITHUB",href="https://github.com/HimesGroup/pargasite",target="_blank")," repository.")))
-))
+                                                    ".csv")),
+                               downloadButton("finalDownload", "Download"),hr(),
+                               h6(p("Greenblatt RE, Himes BE. Facilitating Inclusion of Geocoded Pollution Data into Health Studies. AMIA Jt Summits Transl Sci Proc. 2019;2019:553–561.(PMID:",
+                                    a("31259010",href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6568125/",target="_blank"),").", a("GITHUB",href="https://github.com/HimesGroup/pargasite",target="_blank")," repository."))),
+                             leafletOutput("map", width = "60%",height= 600)
+                    ), 
+                    tabPanel(title = HTML(paste(h3(style = "color:black","MMSA"))), 
+                             sidebarPanel(
+                               h4("Map View"),
+                               selectizeInput(inputId = "year_mmsa",
+                                              label = "Year",
+                                              choices = c("1997":"2021"),
+                                              selected = "2021",
+                                              multiple = FALSE),
+                               selectizeInput(inputId = "pollutant_mmsa",
+                                              label = "Pollutant",
+                                              choices = c("PM2.5", "Ozone", "NO2", "SO2", "CO"),
+                                              select = "PM2.5",
+                                              multiple = FALSE),
+                               p(textOutput("notes_mmsa")), 
+                               h5(textOutput("latlong_mmsa")),
+                               h5(textOutput("pollutant_val_mmsa")),
+                               hr(),
+                               h4("Use the download button to get pollution estimates corresponding to each MMSA and pollutant"),
+                               p("The file returned will have three columns. The first and second column lists the GEOID and names of all the available MMSA and the third column returns the pollutant value corresponding to the MMSA. Change the input parameters (year and pollutant) to download the data for different years and pollutants."),
+                               downloadButton("finalDownload_MMSA", "Download"),hr()
+                             ),
+                             leafletOutput("map_mmsa", width = "60%",height= 600)
+                    ),
+                    tabPanel(title = HTML(paste(h3(style = "color:black", "County"))), 
+                             sidebarPanel(
+                               h4("Map View"),
+                               selectizeInput(inputId = "year_county",
+                                              label = "Year",
+                                              choices = c("1997":"2021"),
+                                              selected = "2021",
+                                              multiple = FALSE),
+                               selectizeInput(inputId = "pollutant_county",
+                                              label = "Pollutant",
+                                              choices = c("PM2.5", "Ozone", "NO2", "SO2", "CO"),
+                                              select = "PM2.5",
+                                              multiple = FALSE),
+                               p(textOutput("notes_county")),
+                               h5(textOutput("latlong_county")),
+                               h5(textOutput("pollutant_val_county")),
+                               hr(),
+                               h4("Use the download button to get pollution estimates corresponding to each county and pollutant"),
+                               p("The file returned will have three columns. The first two columns lists the GOEID and names of all the available counties and the third column returns the pollutant value corresponding to the MMSA. Change the input parameters (year and pollutant) to download the data for different years and pollutants."),
+                               downloadButton("finalDownload_county", "Download"),hr()
+                             ),
+                             leafletOutput("map_county", width = "60%",height= 600)
+                    ),
+                    tabPanel(title = HTML(paste(h3(style = "color:black", "About"))),
+                             includeMarkdown("data/home.md")
+                    ))
+                  
+)
+)
+
+
+
