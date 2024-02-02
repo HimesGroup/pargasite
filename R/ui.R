c93204017c98a97ac501f2e344054a541a273efe
Jaehyun Joo <jaehyunjoo@outlook.com>
2024-02-02 14:12:38 -0500

    Added tab panel to include app information.

--- R/ui.R
+++ R/ui.R
@@ -10,49 +10,97 @@ ui <- fluidPage(
       width = 3
     ),
     mainPanel(
-      ## withSpinner(leafletOutput("us_map", width = "100%", height = 800)),
-      withSpinner(leafletOutput("us_map", height = "67vh")),
-      hr(),
-      radioButtons(
-        inputId = "color",
-        label = span("Color scale",
-                     style = "font-weight: bold"),
-        choices = list("Free", "Fixed"), inline = TRUE
+      tabsetPanel(
+        tabPanel(
+          "Pollutant Map",
+          withSpinner(leafletOutput("us_map", height = "67vh")),
+          hr(),
+          radioButtons(
+            inputId = "color",
+            label = span("Color scale",
+                         style = "font-weight: bold"),
+            choices = list("Free", "Fixed"), inline = TRUE
+          ),
+          tags$div(checkboxInput(
+                 inputId = "color_bounded",
+                 label = "Upper bounded (useful when extreme outliers exist)",
+                 value = FALSE,
+                 width = "100%"
+               ),
+               style = "margin-top: -0.7em; margin-right: 0.5em; display: inline-block"
+               ),
+          tags$head(tags$style(HTML(".not_bold label {font-weight:normal; color: #37474F}"))),
+          tags$div(numericInput(
+                 inputId = "outlier_cutoff",
+                 label = HTML("threshold"),
+                 value = 0,
+                 width = "100%"
+               ), class = "not_bold",
+               style = "margin-top: -2.0em; display: inline-block"),
+          tags$div(tags$p(
+                          "- Free: each data has its own color-scale for the selected year",
+                          tags$br(),
+                          "- Fixed: all data share the same color-scale across the years"
+                        ), style = "color: #900C3F")
+        ),
+        tabPanel(
+          "About",
+          br(),
+          h3("PARGASITE", style = "font-weight: bold"),
+          p("Pollution-Associated Risk Geospatial Analysis SITE (PARGASITE) is",
+            "an R package to offer tools and Shiny application to estimate and",
+            "visualize major pollutant levels (CO, NO2, SO2, Ozone, PM2.5 and PM10)",
+            "covering the conterminous United States at user-defined time ranges.",
+            "It help users to automatically retrieves pollutant data via the",
+            "Environmental Protection Agency’s (EPA) Air Quality System (AQS)",
+            "API service, filters the data by exceptional event (e.g., wildfire)",
+            "status, performs spatial interpolations, and summarizes pollutant",
+            "concentrations by geographic boundaries including State, County, ",
+            "and Core-Based Statistical Area (CBSA)."),
+          p("We have no affiliation with the EPA."),
+          br(),
+          h4("Contributors", style = "font-weight: bold"),
+          p("Jaehyun Joo, Nisha Narayanan, Avantika Diwadkar, Rebecca Greenblatt, and Blanca Himes"),
+          br(),
+          h4("References", style = "font-weight: bold"),
+          p("Greenblatt RE, Himes BE. Facilitating Inclusion of Geocoded Pollution",
+            "Data into Health Studies. AMIA Jt Summits Transl Sci Proc.",
+            "2019;2019:553–561. PMID:",
+            a("31259010",href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6568125/",
+              target="_blank"),
+            ".")
+        )
       ),
-      ## span(
-      ##   tags$div(checkboxInput(
-      ##          inputId = "color_bounded",
-      ##          label = "Upper bounded (useful when extreme outliers exist)",
-      ##          value = FALSE,
-      ##          width = "70%"
-      ##        ), style = "margin-top: -0.7em"),
-      ##   tags$div(numericInput(
-      ##                     inputId = "outlier_cutoff",
-      ##                     label = NULL,
-      ##                     value = 0,
-      ##                     width = "30%"
-      ##                   ), style = "margin-top: -0.7em")
+      ## withSpinner(leafletOutput("us_map", width = "100%", height = 800)),
+      ## withSpinner(leafletOutput("us_map", height = "67vh")),
+      ## hr(),
+      ## radioButtons(
+      ##   inputId = "color",
+      ##   label = span("Color scale",
+      ##                style = "font-weight: bold"),
+      ##   choices = list("Free", "Fixed"), inline = TRUE
       ## ),
-      tags$div(checkboxInput(
-             inputId = "color_bounded",
-             label = "Upper bounded (useful when extreme outliers exist)",
-             value = FALSE,
-             width = "100%"
-           ), style = "margin-top: -0.7em; margin-right: 0.5em; display: inline-block"),
-      tags$head(tags$style(HTML(".not_bold label {font-weight:normal; color: #37474F}"))),
-      tags$div(numericInput(
-             inputId = "outlier_cutoff",
-             label = HTML("threshold"),
-             value = 0,
-             width = "100%"
-           ), class = "not_bold",
-           style = "margin-top: -2.0em; display: inline-block"),
-      ## tags$div(p("cut-off"), style = "display: inline-block"),
-      tags$div(tags$p(
-             "- Free: each data has its own color-scale for the selected year",
-             tags$br(),
-             "- Fixed: all data share the same color-scale across the years"
-           ), style = "color: #900C3F"),
+      ## tags$div(checkboxInput(
+      ##        inputId = "color_bounded",
+      ##        label = "Upper bounded (useful when extreme outliers exist)",
+      ##        value = FALSE,
+      ##        width = "100%"
+      ##      ),
+      ##      style = "margin-top: -0.7em; margin-right: 0.5em; display: inline-block"
+      ##      ),
+      ## tags$head(tags$style(HTML(".not_bold label {font-weight:normal; color: #37474F}"))),
+      ## tags$div(numericInput(
+      ##        inputId = "outlier_cutoff",
+      ##        label = HTML("threshold"),
+      ##        value = 0,
+      ##        width = "100%"
+      ##      ), class = "not_bold",
+      ##      style = "margin-top: -2.0em; display: inline-block"),
+      ## tags$div(tags$p(
+      ##        "- Free: each data has its own color-scale for the selected year",
+      ##        tags$br(),
+      ##        "- Fixed: all data share the same color-scale across the years"
+      ##      ), style = "color: #900C3F"),
       width = 9
     )
   )
