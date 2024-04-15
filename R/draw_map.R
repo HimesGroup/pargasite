aaa0660e46b8b82264adf7a3fa8d736f08cd6ae2
Jaehyun Joo <jaehyunjoo@outlook.com>
2024-04-15 15:13:26 -0400

    Dropped label_fmt argument in .draw_leaflet function

--- R/draw_map.R
+++ R/draw_map.R
@@ -1,6 +1,3 @@
-## Label formatting
-label_fmt <- labelFormat(transform = function(x) sort(x, decreasing = TRUE))
-
 ## Recenter map
 button.js <- paste0("function(btn, map){ map.setView([39.33, -98.58], 4); }")
 
@@ -18,24 +15,24 @@ button.js <- paste0("function(btn, map){ map.setView([39.33, -98.58], 4); }")
     if (length(year) > 1) {
       plist <- lapply(year, function(k) {
         y <- .dimsub(x, dim = "year", value = k, drop = TRUE)
-        .draw_leaflet(y, monitor_dat, min_val, max_val, label_fmt,
+        .draw_leaflet(y, monitor_dat, min_val, max_val,
                       title = paste0("Year: ", k), grid = TRUE)
       })
       do.call(sync, plist)
     } else {
-      .draw_leaflet(x, monitor_dat, min_val, max_val, label_fmt, grid = TRUE)
+      .draw_leaflet(x, monitor_dat, min_val, max_val, grid = TRUE)
     }
   } else {
     if (length(month) > 1) {
       plist <- lapply(month, function(k) {
         y <- .dimsub(x, dim = "month", value = k, drop = TRUE)
-        .draw_leaflet(y, monitor_dat, min_val, max_val, label_fmt,
+        .draw_leaflet(y, monitor_dat, min_val, max_val,
                       title = paste0(month.abb[as.integer(k)], " ", year),
                       grid = TRUE)
       })
       do.call(sync, plist)
     } else {
-      .draw_leaflet(x, monitor_dat, min_val, max_val, label_fmt, grid = TRUE)
+      .draw_leaflet(x, monitor_dat, min_val, max_val, grid = TRUE)
     }
   }
 }
@@ -48,39 +45,42 @@ button.js <- paste0("function(btn, map){ map.setView([39.33, -98.58], 4); }")
     if (length(year) > 1) {
       plist <- lapply(year, function(k) {
         y <- x[x$year == k, ]
-        .draw_leaflet(y, monitor_dat, min_val, max_val, label_fmt,
+        .draw_leaflet(y, monitor_dat, min_val, max_val,
                       title = paste0("Year: ", k), grid = FALSE)
       })
       do.call(sync, plist)
     } else {
-      .draw_leaflet(x, monitor_dat, min_val, max_val, label_fmt, grid = FALSE)
+      .draw_leaflet(x, monitor_dat, min_val, max_val, grid = FALSE)
     }
   } else {
     if (length(month) > 1) {
       plist <- lapply(month, function(k) {
         y <- x[x$month == k, ]
-        .draw_leaflet(y, monitor_dat, min_val, max_val, label_fmt,
+        .draw_leaflet(y, monitor_dat, min_val, max_val,
                       title = paste0(month.abb[as.integer(k)], " ", year),
                       grid = FALSE)
       })
       do.call(sync, plist)
     } else {
-      .draw_leaflet(x, monitor_dat, min_val, max_val, label_fmt,
-                    grid = FALSE)
+      .draw_leaflet(x, monitor_dat, min_val, max_val, grid = FALSE)
     }
   }
 }
 
 ## Underlying function to create an interactive map
-.draw_leaflet <- function(x, monitor_dat, min_val, max_val, label_fmt,
+.draw_leaflet <- function(x, monitor_dat, min_val, max_val,
                           title = NULL, grid = TRUE) {
   p <- leaflet(options = leafletOptions(minZoom = 3)) |>
     addTiles() |>
     setView(lng = -98.58, lat = 39.33, zoom = 4) |>
-    addMarkers(lng = monitor_dat$X, lat = monitor_dat$Y,
-               group = "Monitor Locations") |>
-    addLayersControl(overlayGroups = "Monitor Locations",
-                     options = layersControlOptions(collapsed = FALSE)) |>
+    addMarkers(
+      lng = monitor_dat$X, lat = monitor_dat$Y,
+      group = "Monitor Locations"
+    ) |>
+    addLayersControl(
+      overlayGroups = "Monitor Locations",
+      options = layersControlOptions(collapsed = FALSE)
+    ) |>
     hideGroup("Monitor Locations") |>
     addEasyButton(easyButton(
       icon = "fa-crosshairs", title = "Recenter",
@@ -94,15 +94,22 @@ button.js <- paste0("function(btn, map){ map.setView([39.33, -98.58], 4); }")
       primaryAreaUnit = "sqmeters",
       secondaryAreaUnit = "sqmiles"
     ) |>
-    addLegend(position = "bottomright",
-              pal = .get_pal(min_val, max_val, reverse = FALSE),
-              values = c(min_val, max_val),
-              labFormat = label_fmt, title = title)
+    addLegend(
+      position = "bottomright",
+      pal = .get_pal(min_val, max_val, reverse = FALSE),
+      values = c(min_val, max_val),
+      labFormat = labelFormat(transform = function(x) sort(x, decreasing = TRUE)),
+      title = title
+    )
   if (grid) {
-    p |> addRasterImage(as(x, "Raster"), colors = .get_pal(min_val, max_val),
-                        opacity = 0.6, project = TRUE) |>
-      addPolylines(data = st_transform(getOption("pargasite.map")[["state"]], 4326),
-                   weight = 1, color = "#444444")
+    p |> addRasterImage(
+           as(x, "Raster"), colors = .get_pal(min_val, max_val),
+           opacity = 0.6, project = TRUE
+         ) |>
+      addPolylines(
+        data = st_transform(getOption("pargasite.map")[["state"]], 4326),
+        weight = 1, color = "#444444"
+      )
   } else {
     p |> addPolygons(
       data = x, fillColor = ~.get_pal(min_val, max_val)(value),
